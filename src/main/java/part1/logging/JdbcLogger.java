bed082b8e12a2aba119cdcb7c0181242c6fc961b
Claire Acton <22026811@saultcollege.ca>
2026-03-27 11:28:59 -0400

    jdcbLogger functional

---
+++ src/main/java/part1/logging/JdbcLogger.java
@@ -0,0 +1,60 @@
+package part1.logging;
+
+import java.sql.*;
+import java.time.Instant;
+
+public class JdbcLogger extends ConsoleLogger {
+
+   private static String dbUrlString;
+   Connection connection;
+
+   //constructor with url of destination db.
+   public JdbcLogger(String url) {
+
+     this.dbUrlString = url;
+
+       try {
+           this.connection = DriverManager.getConnection(this.dbUrlString);
+       } catch (SQLException e) {
+           throw new RuntimeException("Error occured trying to connect.");
+       }
+   }
+//TODO:do i want this to be broadly applicable (any database) or just for this database???
+    /**i mean it's likely it's only applicable to this one database. how else would
+     * we know where to insert what values?? there's probably a way to populate left-to-right... I"d be
+     * really surprised if no one had done that.
+     *connection metadata....
+     */
+
+    @Override
+    public void log(String message, LogLevel logLevel) {
+        //things to check:
+        //does database have a table?
+        //checking to see if table has enough columns to store the log.
+        //ideally will get column names and table names from the database info.
+        //just testing to see if it works in the first place.
+        try {
+            var v = this.connection.getMetaData();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        try {
+            PreparedStatement s = connection.prepareStatement(
+                    "INSERT INTO main.log_entries (timestamp, level, message) VALUES (?, ?, ?);");
+            s.setString(1, Instant.now().toString());
+            s.setString(2, logLevel.toString());
+            s.setString(3, message);
+
+            s.execute();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
+
