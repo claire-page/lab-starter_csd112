a4c100838339ffc28ad4089c4b25fddb2c018814
Claire Acton <22026811@saultcollege.ca>
2026-04-03 18:58:47 -0400

    forgot to commit for 2 days and broke stuff. HERE THINGS WORK.

--- src/main/java/ui/Qwerty.java
+++ src/main/java/ui/Qwerty.java
@@ -1,32 +1,29 @@
 package ui;
 
 import core.ReplaceableText;
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.FlowPane;
-
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 
 import javafx.scene.text.Font;
 
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 //TODO: GET TEXT TO WRAP PROPERLY.
 
-import java.util.Timer;
-
 import static controllers.Controller.typeText;
+import static javafx.scene.text.Font.font;
 
 public class Qwerty extends javafx.application.Application {
 
@@ -38,14 +35,17 @@ public class Qwerty extends javafx.application.Application {
 
         Font DEFAULT_FONT = new Font("Consolata", 30);
 
-        TxtColourScheme DEFAULT_SCHEME = new TxtColourScheme("rgb(160,174,188)",
-                "rgb(67,97,117)",
-                "rgb(126, 25, 27)",
+        TxtColourScheme DEFAULT_SCHEME = new TxtColourScheme("rgb(140, 167, 174)",
+                "rgb(16, 31, 36)",
+                "rgb(255,79,90)",
                 "rgb(175, 64, 53)");
 
         this.pref = new Preferences(DEFAULT_FONT, DEFAULT_SCHEME);
 
         stage.setScene(getInitializedHomeScene(pref, stage));
+        stage.setWidth(1000);
+        stage.setHeight(670);
+        stage.setResizable(false);
         stage.show();
     }
 
@@ -61,26 +61,29 @@ public class Qwerty extends javafx.application.Application {
      * @return
      */
     public FlowPane initPane( QChar[] chars, ReplaceableText rt, Preferences pref){
-
+///
         FlowPane p = new FlowPane();
-        p.setPadding(new Insets(70));
-        p.setPrefSize(500,        Defaults.DEFAULT_PANEWIDTH);
+        p.setPadding(new Insets(20));
 
         HBox [] holder = new HBox[rt.getWordCount()];
-        for (int i = 0; i< holder.length; i++) {
+        for (int i = 0; i < holder.length; i++) {
             holder[i] = new HBox();
         }
+
         int j = 0;
         for (int k = 0; k < chars.length; k++) {
             var q = chars[k];
             q.setFill(pref.colours().getBlankTextColour());
             q.setFont(pref.font());
             holder[j].getChildren().add(q);
-            if (q.valueOf().equals(" ") || q.valueOf().matches("\\p{P}") || k == chars.length-1){ //we reach punctuation or we end up at the end...
-                p.getChildren().add(holder[j]); //adding hBox.
+            if ((q.valueOf().equals(" ") || q.valueOf().matches("\\p{P}") || k == chars.length-1)){ //we reach punctuation or we end up at the end...
+                p.getChildren().add(holder[j]);
                 j+=1;
+                //adding hBox.
             }
         }
+        p.setFocusTraversable(true);
+        p.setBackground(Background.fill(Defaults.DEFAULT_TXT_BKGRND));
         return(p);
     }
 
@@ -89,8 +92,6 @@ public class Qwerty extends javafx.application.Application {
      * @param pref
      * @return
      */
-
-
 
     public Scene getInitializedGameScene(Preferences pref, Stage stage){
 
@@ -106,14 +107,19 @@ public class Qwerty extends javafx.application.Application {
             qChars[i] = new QChar(splitText[i]);
         }
         var pane = initPane(qChars, text, pref ); //initializing textpane with qChars.
-        pane.setFocusTraversable(true);
 
         VBox v = new VBox();
-        Button restartBtn = new Button("Restart");
+        Button restartBtn = new Button("_Restart");
+        restartBtn.setFont(Defaults.DEFAULT_MENU_FONT);
+        restartBtn.setBackground(Background.fill(Defaults.DEFAULT_ACCENT_1));
+
         restartBtn.setPadding(new Insets(5));
 
-        Button quitBtn = new Button("Quit");
+        Button quitBtn = new Button("_Quit");
+        quitBtn.setFont(Defaults.DEFAULT_MENU_FONT);
+        quitBtn.setBackground(Background.fill(Defaults.DEFAULT_ACCENT_1));
         quitBtn.setPadding(new Insets(5));
+
         EventHandler<MouseEvent> handler1 = e -> {
             stage.setScene(getInitializedHomeScene(pref, stage));
         };
@@ -122,17 +128,19 @@ public class Qwerty extends javafx.application.Application {
         Label timerDisplay = new Label("");
 
         //TODO: GET TIMER TO DISPLAY ACCURATE VALUE...
-//
 
         v.getChildren().add(restartBtn);
         v.getChildren().add(quitBtn);
         v.setPrefHeight(Defaults.DEFAULT_SCENEHEIGHT);
+        v.setPrefWidth(250);
         v.setPadding(new Insets(10));
         v.setSpacing(100);
+        v.setBackground(Background.fill(Defaults.DEFAULT_ACCENT_1));
 
         HBox h = new HBox();
         h.getChildren().addAll(v, pane, timerDisplay);
-        Scene s = new Scene(h, 900, 900);
+        Scene s = new Scene(h, 1000, 670);
+        s.setFill(Defaults.DEFAULT_BKGROUND);
 
         EventHandler<KeyEvent> handler = event -> typeText(event, text, pref, qChars);
         s.addEventFilter(KeyEvent.ANY, handler);
@@ -142,8 +150,57 @@ public class Qwerty extends javafx.application.Application {
 
 //have two methods repeating similar steps.
     public Scene getInitializedHomeScene(Preferences pref, Stage stage){
-        ReplaceableText text = new ReplaceableText("Welcome to this type test! Lorem ipsum blah blah blah...texty texty texty....ajgflweafljewbfaerjhferwlafhere!");
-        System.out.println(text.toString().length());
+
+
+        //INITIALIZING LEFT SIDE OF SCREEN... (MENU)
+        VBox v = new VBox();
+        Button newGame = new Button("_Play");
+        newGame.setPadding(new Insets(100, 5, 5, 25));
+        newGame.setFont(font("Consolas", FontWeight.BOLD, 24));
+
+        newGame.setTextFill(Defaults.DEFAULT_TXT_BKGRND);
+        newGame.setBackground(Background.fill(Defaults.DEFAULT_ACCENT_1));
+
+        Button settings = new Button("_Settings");
+        settings.setPadding(new Insets(5, 5, 5, 20));
+        settings.setFont(Defaults.DEFAULT_MENU_FONT);
+        settings.setTextFill(Defaults.DEFAULT_TXT_BKGRND);
+        settings.setBackground(Background.fill(Defaults.DEFAULT_ACCENT_1));
+
+        Button about = new Button("_About");
+        about.setPadding(new Insets(5, 5, 5, 20));
+        about.setFont(Defaults.DEFAULT_MENU_FONT);
+        about.setTextFill(Defaults.DEFAULT_TXT_BKGRND);
+        about.setBackground(Background.fill(Defaults.DEFAULT_ACCENT_1));
+
+        v.getChildren().add(newGame);
+        v.getChildren().add(settings);
+        v.getChildren().add(about);
+        v.setPrefHeight(Defaults.DEFAULT_SCENEHEIGHT);
+        v.setPrefWidth(200);
+        v.setMaxWidth(200);
+        v.setMinWidth(200);
+        v.setSpacing(70);
+        v.setBackground(Background.fill(Defaults.DEFAULT_ACCENT_1));
+
+        //INITIALIZING CONTENT FOR RIGHT SIDE
+        Font bigFont = font("Courier New", 80);
+        Label titletext = new Label("Q W E R T Y");
+        titletext.setTextFill(Defaults.DEFAULT_CONTRAST_ACCENT_LT);
+        titletext.setFont(bigFont);
+        titletext.setPadding(new Insets(50, 0, 0, 0));
+        titletext.setAlignment(Pos.BASELINE_LEFT);
+
+        Label bottomtitle = new Label(" A JavaFX app for CSD112-26W");
+        bottomtitle.setBackground(Background.fill(Defaults.DEFAULT_CONTRAST_ACCENT_DK));
+        bottomtitle.setFont(new Font("Consolas", 18));
+        bottomtitle.setTextFill(Defaults.DEFAULT_CONTRAST_LIGHT_RED);
+        bottomtitle.setAlignment(Pos.BASELINE_LEFT);
+        bottomtitle.setPrefWidth(750);
+        bottomtitle.setPrefHeight(40);
+
+        //SETTING UP REPLACEABLE TEXT (ALWAYS THE SAME FOR HOME SCREEN.)
+        ReplaceableText text = new ReplaceableText("Welcome to this type test! Lorem ipsum blah blah blah...texty texty texty....ajgflweafljewbfaerjhferwlafhere! More text, even more text. Words and nouns and adjectives. TEXTYDSYTUFGIUHIHDIHLISD");
 
         QChar[] qChars = new QChar[text.getText().length()];
 
@@ -152,35 +209,35 @@ public class Qwerty extends javafx.application.Application {
         for (int i= 0; i< splitText.length; i++){
             qChars[i] = new QChar(splitText[i]);
         }
-        var pane = initPane(qChars, text, pref );
-        pane.setFocusTraversable(true);
 
-        VBox v = new VBox();
-        Button newGame = new Button("Play");
-        newGame.setPadding(new Insets(5));
+        //INITIALIZING CUSTOM FLOWPANE CONTAINING QCHARS
+        var txtpane = initPane(qChars, text, pref );
 
+        //INITIALIZING PANE FOR RIGHT SIDE
+        VBox rightpane = new VBox();
+        rightpane.setSpacing(20);
+        rightpane.setPrefHeight(Defaults.DEFAULT_SCENEHEIGHT);
+        rightpane.getChildren().addAll(titletext, bottomtitle, txtpane);
+        rightpane.setPadding(new Insets(0, 40, 0, 40));
+
+        //PUTTING IT ALL IN A BORDERPANE.
+        BorderPane borderPane = new BorderPane();
+        borderPane.setLeft(v);
+        BorderPane.setAlignment(titletext,Pos.BASELINE_LEFT );
+        borderPane.setCenter(rightpane);
+        borderPane.setBackground(Background.fill((Defaults.DEFAULT_BKGROUND)));
+        Scene s = new Scene(borderPane, 1000, 670);
+        s.setFill(Defaults.DEFAULT_BKGROUND);
+
+        //ADDING EVENT HANDLERS---
+        //this has to be added at the scene level...
+        EventHandler<KeyEvent> handler = event -> typeText(event, text, pref, qChars);
+        s.addEventFilter(KeyEvent.ANY, handler);
+
+        //button clicks.
         EventHandler<MouseEvent> handler0 =  event -> stage.setScene(getInitializedGameScene(pref, stage));
         newGame.addEventHandler(MouseEvent.MOUSE_CLICKED, handler0);
 
-
-        Button settings = new Button("Settings");
-        settings.setPadding(new Insets(5));
-        Button about = new Button("About");
-        about.setPadding(new Insets(5));
-
-
-        v.getChildren().add(newGame);
-        v.getChildren().add(settings);
-        v.getChildren().add(about);
-        v.setPrefHeight(Defaults.DEFAULT_SCENEHEIGHT);
-        v.setMaxWidth(Defaults.DEFAULT_PANEWIDTH);
-        v.setPadding(new Insets(10));
-        v.setSpacing(100);
-        HBox h = new HBox();
-        h.getChildren().addAll(v, pane);
-        Scene s = new Scene(h, 900, 900);
-        EventHandler<KeyEvent> handler = event -> typeText(event, text, pref, qChars);
-        s.addEventFilter(KeyEvent.ANY, handler);
 
         return(s);
     }
